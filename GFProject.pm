c81418ef8f993db0d7e105685f8c394db58baa99
Brian Stevens <brian@stevens.com>
2007-11-15 21:29:08 +0000

    Merged bds-devel into the trunk

---
+++ GFProject.pm
@@ -0,0 +1,91 @@
+package GFProject;
+
+use strict;
+use DBI;
+
+##
+## Class variables
+##
+my $_dbh;
+
+##
+## Public class methods
+##
+
+sub END {
+	$_dbh->disconnect if $_dbh
+}
+
+##
+## Public instance methods
+##
+
+sub connect {
+	my $caller = shift;
+	my $self = {};
+	$self->{"cache"} = {};
+	my ($proj, $pass) = @_;
+	my $dbh = _GetDbHandle($proj, $pass);
+	bless $self, ref $caller || $caller
+}
+
+sub disconnect {
+	my $self = shift;
+	$_dbh->disconnect
+}
+
+sub getAllUwis {
+	my $self = shift;
+	return @{$self->{"cache"}->{"getAllUwis"}} if $self->{"cache"}->{"getAllUwis"};
+	my $sql = "select uwi from borehole";
+	map {push @_, $_->[0]} @{$_dbh->selectall_arrayref($sql)};
+	@{$self->{"cache"}->{"getAllUwis"}} = sort {$b cmp $a} @_;
+	return @{$self->{"cache"}->{"getAllUwis"}}
+}
+
+sub getCheckshotsByUwi {
+	my $self = shift;
+	my $sql = "select w.id from well_check_shot_survey w, borehole b where w.container_id = b.id and w.container_table = 'Borehole' and b.uwi = ?";
+	map {push @_, $_->[0]} @{$_dbh->selectall_arrayref($sql, {}, shift)};
+	return @_
+}
+
+sub getDuplicateUwis {
+	my $self = shift;
+	return @{$self->{"cache"}->{"getDuplicateUwis"}} if $self->{"cache"}->{"getDuplicateUwis"};
+	my @all = $self->getAllUwis;
+	for (1 .. $#all) {
+		if ($all[$_ - 1] =~ m/$all[$_]/) {
+			push @_, $all[$_ - 1] unless $all[-1] eq $all[$_ - 1];
+			push @_, $all[$_]
+		}
+	}
+	@{$self->{"cache"}->{"getDuplicateUwis"}} = @_;
+	return @{$self->{"cache"}->{"getDuplicateUwis"}}
+}
+
+sub getDeviationsByUwi {
+	my $self = shift;
+	my $sql = "select w.id from well_deviation_survey w, borehole b where w.container_id = b.id and w.container_table = 'Borehole' and b.uwi = ?";
+	map {push @_, $_->[0]} @{$_dbh->selectall_arrayref($sql, {}, shift)};
+	return @_
+}
+
+sub getMarkersByUwi {
+	my $self = shift;
+	my $sql = "select distinct e.entity_id from e_to_rr e, borehole b where e.implementation_table in ('Biostrat_Marker','Chronostrat_Marker','Fault_Marker','Fluid_Marker','SedMarker','Strat_Marker','Struct_Marker','Struct_XSect_Marker','Well_Marker') and e.entity_table = 'Borehole' and e.property_order = 0 and e.entity_id = b.id and b.uwi = ?";
+	map {push @_, $_->[0]} @{$_dbh->selectall_arrayref($sql, {}, shift)};
+	return @_
+}
+
+##
+## Private class methods
+##
+
+sub _GetDbHandle {
+	my $proj = shift;
+	my $pass = shift;
+	$_dbh = DBI->connect_cached("DBI:Oracle:", $proj, $pass, {"AutoCommit" => 1})
+}
+
+1;
