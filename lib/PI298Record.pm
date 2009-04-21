88ccae33f7169d34de739d4595175de39fccce33
Brian Stevens <brian@dataporters.com>
2009-04-21 13:57:25 -0500

    Added ability to parse production data for cumulative liquid and gas.   (Still need to add in the parsing of 297 format for reserve, depth, etc.)

--- lib/PI298Record.pm
+++ lib/PI298Record.pm
@@ -14,6 +14,24 @@ sub api {
 	return $self->{"api"}
 }
 
+sub cumulative_production {
+	my $self = shift;
+	return $self->{"cumulative_production"} if exists $self->{"cumulative_production"};
+	my @stats;
+	my ($year, $liquid, $gas);
+	for (split /\n/, $self->{"string"}) {
+		/^\+F (\d{4})\s+(\d{1,20})\s+(\d{1,20})/;
+		$year = $1;
+		if ($year) {
+			$liquid = $2 ? $2 : ($2 == 0 ? $2 : '');
+			$gas = $3 ? $3 : ($3 == 0 ? $3 : '');
+			@{$self->{"cumulative_production"}} = ($year, $liquid, $gas)
+		}
+	}
+	@{$self->{"cumulative_production"}} = ('', '', '') unless defined $self->{"cumulative_production"};
+	return $self->{"cumulative_production"}
+}
+
 sub new {
 	my $caller = shift;
 	my $self = {"string" => shift};
